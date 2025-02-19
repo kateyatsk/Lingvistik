71a6741f52af57bbb3d6aba0408d03ab930e9dc5
kateyatsk <klaettoe2023@gmail.com>
2025-02-19 11:29:04 +0300

    Initial Commit

---
+++ Lingvistik/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lingvistik
+//
+//  Created by Екатерина Яцкевич on 19.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lingvistik/LingvistikApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingvistikApp.swift
+//  Lingvistik
+//
+//  Created by Екатерина Яцкевич on 19.02.25.
+//
+
+import SwiftUI
+
+@main
+struct LingvistikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
